37cedca9f836fd887c7983dc246c7f4302cf88b3
artemmatiushenko1 <artom.matyushenko@gmail.com>
2022-06-26 17:21:21 +0300

    Implemented EventLoop and defined methods Post, Execute and AwaitFinish

---
+++ engine/eventLoop.go
@@ -0,0 +1,43 @@
+package engine
+
+type Handler interface {
+	Post(cmd Command)
+}
+
+type EventLoop struct {
+	eq         *EventsQueue
+	shoudlStop bool
+	stopSignal chan struct{}
+}
+
+func (loop *EventLoop) Start() {
+	loop.eq = &EventsQueue{
+		notEmpty: make(chan struct{}),
+	}
+
+	loop.stopSignal = make(chan struct{})
+
+	go func() {
+		for !loop.shoudlStop || !loop.eq.isEmpty() {
+			cmd := loop.eq.pull()
+			cmd.Execute(loop)
+		}
+
+		loop.stopSignal <- struct{}{}
+	}()
+}
+
+func (loop *EventLoop) Post(cmd Command) {
+	// l.q.push(cmd)
+}
+
+type stopCommand struct{}
+
+func (scmd stopCommand) Execute(h Handler) {
+	h.(*EventLoop).shoudlStop = true
+}
+
+func (loop *EventLoop) AwaitFinish() {
+	loop.Post(stopCommand{})
+	<-loop.stopSignal
+}
